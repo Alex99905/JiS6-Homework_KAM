58485020aa36b0c044566a42941e8bea896d865b
Svetlana101010 <konduhowich@gmail.com>
2021-03-07 00:17:26 +0300

    MathOperationHasBeanDone

---
+++ JavaHomework/src/main/java/io/github/Alex99905/MathOperation/MathOperation.java
@@ -0,0 +1,22 @@
+package io.github.Alex99905.MathOperation;
+
+public class MathOperation {
+
+
+    public static int substraction(int a, int b) {
+        return a - b;
+    }
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public static int division(int a, int b) {
+        return a / b;
+    }
+
+}
